a95f05f0f6b2ce39faaedd2fff705e92ea2b13d2
Andy <andyecgt@gmail.com>
2023-03-15 11:34:06 +0800

    加入專案檔案。

---
+++ test_SOLID/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ * SOLID 指導原則
+ * S (Single Responsibility Principle): 單一責任原則 SRP
+ * O (Open Closed Principle): 開放封閉原則 OCP
+ * L (Liskov Substitution Principle): 里氏替換原則 LSP
+ * I (Interface Segregation Principle): 介面隔離原則 ISP
+ *  介面隔離原則要求我們應該根據使用者的需求來設計介面，而不是將所有的方法都放在一個大型的介面中
+ * D (Dependence Inversion Principle): 相依反轉原則 DIP
+ */
+
---
+++ test_SOLID/SRP.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_SOLID
+{
+    public class SRP
+    {
+        /*
+         * 單一責任原則:
+         * 要求每個類別都應該只有一個職責。
+         * 這樣做可以讓類別更容易維護，並且可以降低類別間的相依性。
+         * 
+         * 高聚合力，提升重用性質。
+         */
+
+        #region ex1
+        public class User
+        {
+            public string Name = "";
+            public string Password = "";
+        }
+
+        public class UserService
+        {
+            public void AddUser(User user)
+            {
+                // 檢查使用者資訊是否符合規定
+                if (IsValidUser(user))
+                {
+                    // 將使用者加入資料庫
+                    new UserRepository().Add(user);
+                }
+            }
+
+            private bool IsValidUser(User user)
+            {
+                // 檢查使用者名稱、密碼等資訊是否符合規定
+                // 如果資訊不符合規定，回傳 false
+                // 如果資訊符合規定，回傳 true
+                bool flag = false;
+                return flag;
+            }
+        }
+
+        public class UserRepository 
+        {
+            public void Add(User s)
+            {
+                //將使用者加到資料庫
+            }
+        }
+        #endregion
+
+        #region ex2
+        public class Product
+        {
+            //處理產品相關
+        }
+
+        public class Order
+        {
+            //處理訂單相關
+        }
+
+        public class Sales
+        {
+            //處理業務相關
+        }
+        #endregion
+    }
+}
